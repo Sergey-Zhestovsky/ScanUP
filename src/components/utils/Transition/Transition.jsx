bbe5514cf34dd5b661ce0a849d6f1d386d4da15b
Sergey Zhestovsky <Sergey.play123@gmail.com>
2020-02-12 13:40:24 +0200

    modify Transition: add delay method

--- src/components/utils/Transition/Transition.jsx
+++ src/components/utils/Transition/Transition.jsx
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { CSSTransition } from "react-transition-group";
 
 import stylesFade from "./transition-fade.module.less";
 import stylesHop from "./transition-hop.module.less";
+import stylesSoftHop from "./transition-soft-hop.module.less";
 
 export const TRANSITION_STYLES = {
   FADE: stylesFade,
-  HOP: stylesHop
+  HOP: stylesHop,
+  SOFT_HOP: stylesSoftHop
 };
 
-export default function Transition(props) {
-  let {
-    timeout = 200,
-    style,
-    ...rest
-  } = props;
+export default class Transition extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <CSSTransition
-      timeout={timeout}
-      classNames={timeout === 0 ? {} : style}
-      {...rest}>
-      {props.children}
-    </CSSTransition>
-  );
+    this.state = {
+      show: props.in,
+      timer: null
+    };
+  }
+
+  componentDidUpdate() {
+    if (!this.props.delay)
+      return;
+
+    if (this.state.show !== this.props.in && this.state.timer === null) {
+      this.setTimer();
+    }
+  }
+
+  setTimer() {
+    clearTimeout(this.state.timer);
+    this.setState({
+      timer: setTimeout(() => this.setState({
+        show: this.props.in,
+        timer: null
+      }), this.props.delay ?? 0)
+    })
+  }
+
+  render() {
+    let {
+      timeout = 200,
+      style,
+      delay,
+      children,
+      ...rest
+    } = this.props;
+
+    return (
+      <CSSTransition
+        timeout={timeout}
+        classNames={timeout === 0 ? {} : style}
+        {...rest}
+        in={delay ? this.state.show : this.props.in}
+      >
+        {children}
+      </CSSTransition>
+    );
+  }
 }
