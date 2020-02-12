0f16ffa27b08ee46b760b4ff4bdcfcfec1e04f0e
Sergey Zhestovsky <Sergey.play123@gmail.com>
2020-02-12 13:41:57 +0200

    mdify Condition: rework component as decorator func

--- src/components/parts/Condition/Condition.jsx
+++ src/components/parts/Condition/Condition.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Condition(props) {
+export default function Decorator(props) {
+  function decorator() {
+    let {
+      visible,
+      mounted,
+      ...rest
+    } = props,
+      result = props.children;
+
+    if (visible !== undefined) {
+      //let Temp = with (result, visible);
+      result = <Visible visible={visible} {...rest}>{result}</Visible> //<Temp {...rest} />;
+    }
+    if (mounted !== undefined)
+      result = <Mounted mounted={props.mounted}>{result}</Mounted>;
+
+    return result;
+  }
+
+  return decorator();
+}
+
+function Mounted(props) {
   let {
     mounted = true,
     children
@@ -10,4 +32,39 @@ export default function Condition(props) {
     return children;
 
   return null;
+}
+
+class Visible extends Component {
+  constructor(props) {
+    super(props);
+    this.wrapperRef = React.createRef();
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll);
+  }
+
+  isVisible() {
+    window.removeEventListener("scroll", this.onScroll);
+    return this.props.visible();
+  }
+
+  onScroll = () => {
+    if (!this.wrapperRef.current)
+      return;
+
+    let midScreen = window.scrollY + window.innerHeight / 2,
+      wrapperTop = this.wrapperRef.current.offsetTop;
+
+    if (midScreen > wrapperTop)
+      return this.isVisible();
+  };
+
+  render() {
+    return (
+      <div ref={this.wrapperRef}>
+        {this.props.children}
+      </div>
+    );
+  }
 }
